8b0046362f88c512addb2a3845dc6ad27b2b1c19
ajimnav1901 <151150960+ajimnav1901@users.noreply.github.com>
2024-01-19 11:48:13 +0100

    Add files via upload

---
+++ actividad 4/js/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let a,b,c,d,e,f,g;
+    a = parseInt(prompt("Inserta un numero"));
+    b= parseInt(prompt("Inserta el siguiente numero"));
+    c= a + b;
+    d= a - b;
+    e= a * b
+    f= a / b
+    g= a % b
+    alert ("El primer numero es: " + a + "\nEl segundo numero es:" + b + "\nLa suma es: " + c + "\nLa resta es: " + d + "\nEl producto es: " + e + "\nLa division es: " + f + "\nEl resto es: " + b  );
+}

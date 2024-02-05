3d4abcc64ff1c5783de467a23ff7a91f958a71cf
ajimnav1901 <151150960+ajimnav1901@users.noreply.github.com>
2024-02-05 13:28:38 +0100

    Add files via upload

---
+++ actividad 7/js/script.js
@@ -0,0 +1,14 @@
+function comprobar() { let nota;
+    nota=parseInt(document.getElementById("nota").value, 10);
+    if (nota >= 7 && nota < 9) 
+        document.getElementById ("mensaje").innerHTML="Notable";
+    if (nota >= 5 && nota < 6)
+        document.getElementById ("mensaje").innerHTML="suficiente";
+    if (nota >= 6 && nota < 7)
+        document.getElementById ("mensaje").innerHTML="Bien";
+    if (nota >=0 && nota < 5)
+        document.getElementById ("mensaje").innerHTML="Suspenso";
+    if (nota >= 9 && nota <= 10)
+        document.getElementById ("mensaje").innerHTML="Sobresaliente";
+}
+    
